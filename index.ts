12409df69ba7ffea277fad4ce881fb5794f63c34
YASSINEBRIKI <brikiy418@gmail.com>
2022-06-09 11:50:54 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //###############################################################
-let nun1, nun2, result: number;
-nun1 = parseInt(prompt('entrez le number'));
-nun2 = parseInt(prompt('entrez le number'));
-if (nun1 < nun2) {
-  console.log(nun1 - nun2);
-}
-if (nun1 > nun2) {
-  console.log(nun2 - nun1);
+
+let nun1, result: number;
+nun1 = parseInt(prompt('ادخل اعدد'));
+
+result = nun1 % 2;
+if (result == 0) {
+  console.log('العدد زوجي ');
+} else {
+  console.log('العدد زوجي');
 }
